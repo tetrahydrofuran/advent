9f58caa82e7ceacdbbf58cea365a437d9d290537
lzhou <lzhou95@gmail.com>
2020-10-02 23:22:39 -0700

    2.2

--- 2017/day2.py
+++ 2017/day2.py
@@ -2,6 +2,20 @@ def checksum1(matrix):
     return sum(max(line) - min(line) for line in matrix)
 
 
+def checksum2(matrix):
+    # Brute force to find the divisible numbers; not sure if there's a great way to search this
+    counter = 0
+    for row in matrix:
+        for i in range(len(row)):
+            for j in range(len(row)):
+                if i == j:  # Don't check self
+                    continue
+                if row[i] / row[j] == row[i] // row[j]:
+                    counter += row[i] // row[j]
+                    break
+    return counter
+
+
 if __name__ == '__main__':
     with open('files/2.txt') as f:
         input_ = f.readlines()
@@ -9,3 +23,6 @@ if __name__ == '__main__':
 
     assert checksum1([[5, 1, 9, 5], [7, 5, 3], [2, 4, 6, 8]]) == 18
     print('Part 1:', checksum1(input_))
+
+    assert checksum1([[5, 9, 2, 8], [9, 4, 7, 3], [3, 8, 6, 5]]) == 18
+    print('Part 2:', checksum2(input_))
