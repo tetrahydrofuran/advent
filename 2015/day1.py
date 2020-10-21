bd7134b802490272e296aa112b4fb2cffd410d4d
lzhou <lzhou95@gmail.com>
2020-10-20 22:55:32 -0700

    2015.1.2

--- 2015/day1.py
+++ 2015/day1.py
@@ -3,10 +3,22 @@ from collections import Counter
 
 def part1(paren):
     """
-    +1 for open paren, -1 for close paren
+    Bulk calculation +1 for open paren, -1 for close paren
     """
     c = Counter(paren)
     return c['('] - c[')']
+
+
+def part2(paren):
+    """
+    Take iterable approach to counting floors, break when -1
+    """
+    floor = 0
+    for i, p in enumerate(paren):
+        floor += 1 if p == '(' else -1
+        if floor == -1:
+            return i + 1  # 1-indexed position
+    # Assume will always enter the basement
 
 
 if __name__ == '__main__':
@@ -16,3 +28,4 @@ if __name__ == '__main__':
     assert part1('(())()()') == 0
     assert part1(')())())') == -3
     print(part1(input_))
+    print(part2(input_))
