6a385924cf2a42627c3518e92e0d7aa62c790b33
lzhou <lzhou95@gmail.com>
2020-12-05 15:53:15 -0800

    2020.4.2

--- 2020/day4.py
+++ 2020/day4.py
@@ -24,9 +24,81 @@ def part1(passports, optional=('cid',)):
     return valid
 
 
+def parse_passports(passports):
+    parsed_passports = []
+    for p in passports:
+        parsed = {}
+        p = p.strip().split(' ')
+        for kv in p:
+            key, value = kv.split(':')
+            parsed[key] = value
+        parsed_passports.append(parsed)
+    return parsed_passports
+
+
+def part2(parsed_passports):
+    valid = 0
+    for p in parsed_passports:
+        try:
+            conditions = [
+                check_year(p['byr'], 1920, 2002),
+                check_year(p['iyr'], 2010, 2020),
+                check_year(p['eyr'], 2020, 2030),
+                check_height(p['hgt']),
+                check_hair(p['hcl']),
+                p['ecl'] in ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'),  # Check eyes
+                p['pid'].isnumeric() and len(p['pid']) == 9,  # Passport ID check
+            ]
+            print(p)
+            print(conditions)
+            valid += all(conditions)
+        except KeyError:
+            continue  # Missing field or failed to satisfy
+
+    return valid
+
+
+# region Checks
+def check_year(value, miny, maxy):
+    # Four-digit check is implicit from provided miny, maxy
+    try:
+        value = int(value)
+        return miny <= value <= maxy
+    except ValueError:
+        return False
+
+
+def check_height(height):
+    try:
+        if 'cm' == height[-2:]:
+            height = int(height.replace('cm', ''))
+            return 150 <= height <= 193
+        elif 'in' == height[-2:]:
+            height = int(height.replace('in', ''))
+            return 59 <= height <= 76
+    except ValueError:  # Not assuming all inputs are well-formed
+        pass
+    return False
+
+
+def check_hair(hcl):
+    if len(hcl) != 7 or hcl[0] != '#':
+        return False
+    hcl = hcl[1:]
+    for ch in hcl:
+        if not (48 <= ord(ch) <= 57 or 97 <= ord(ch) <= 102):  # 0-9, a-f
+            return False
+    return True
+
+
+# endregion
+
 if __name__ == '__main__':
     with open('files/4.txt') as f:
         input_ = f.readlines()
 
     pp = get_passports(input_)
+    print(pp)
     print(part1(pp))
+    pp = parse_passports(pp)
+    print(part2(pp))
