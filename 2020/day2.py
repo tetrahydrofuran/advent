b30418af15c62ff7ad53b81016605cf72edf45f6
lzhou <lzhou95@gmail.com>
2020-12-05 08:49:21 -0800

    2020.2.2

--- 2020/day2.py
+++ 2020/day2.py
@@ -12,11 +12,24 @@ def clean_input(string):
 
 
 def part1(inputs):
+    """Letter frequency interpretation of password rules."""
     valid = 0
     for inp in inputs:
         mi, ma, ch, pw = clean_input(inp)
         c = Counter(pw)
         if mi <= c[ch] <= ma:
+            valid += 1
+    return valid
+
+
+def part2(inputs):
+    """Letter positioning interpretation of password rules."""
+    valid = 0
+    for inp in inputs:
+        p1, p2, ch, pw = clean_input(inp)
+        condition1 = pw[p1 - 1] == ch
+        condition2 = pw[p2 - 1] == ch
+        if condition1 != condition2:  # XOR
             valid += 1
     return valid
 
@@ -27,3 +40,4 @@ if __name__ == '__main__':
 
     print(clean_input('1-3 a: abcde'))
     print(part1(input_))
+    print(part2(input_))
