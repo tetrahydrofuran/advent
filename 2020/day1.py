d3010a7fa156e123f82ad4ff7a6d1295c33e947d
lzhou <lzhou95@gmail.com>
2020-12-05 08:04:37 -0800

    2020.1.2

--- 2020/day1.py
+++ 2020/day1.py
@@ -5,8 +5,17 @@ def part1(inputs):
                 return j * k
 
 
+def part2(inputs):
+    for i, n1 in enumerate(inputs):
+        for j, n2 in enumerate(inputs[i:]):
+            for k, n3 in enumerate(inputs[j:]):
+                if n1 + n2 + n3 == 2020:
+                    return n1 * n2 * n3
+
+
 if __name__ == '__main__':
     with open('files/1.txt') as f:
         input_ = f.readlines()
         input_ = [int(i.strip()) for i in input_]
     print(part1(input_))
+    print(part2(input_))
